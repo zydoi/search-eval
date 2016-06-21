dd59366194d2ce7037a1c7fdb9e455e1a5294fee
Ethan Zhang <zydoi@163.com>
2016-06-21 18:10:30 +0800

    Init web module

--- src/main/java/com/litb/search/eval/controller/AnnotateController.java
+++ src/main/java/com/litb/search/eval/controller/AnnotateController.java
@@ -1,8 +1,17 @@
 package com.litb.search.eval.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class AnnotateController {
 
+	@RequestMapping(value="/helloworld", method=RequestMethod.GET)
+	public String helloworld(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+		return "greeting";
+	}
 }
